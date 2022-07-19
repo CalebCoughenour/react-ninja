be5987a9fb2a7eff71a21b09b36a293c00bc4054
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-19 15:52:35 -0700

    begin adding nav bar with home page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+
+
 
 function App() {
   return (
     <div className="App">
-      <div>
-        Hello
+      <Navbar />
+      <div className='conted'>
+        <Home />
       </div>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <h1>Belac Blog
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New Blog</a>
+        </div>
+      </h1>
+    </nav>
+   );
+}
+ 
+export default Navbar;
