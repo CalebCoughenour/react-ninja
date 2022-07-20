150c52b9aa5941ac91bf762e9d09a20475761c69
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-20 16:48:03 -0700

    add catch block for error and a useState with conditional for error

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,31 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
-
+  const [error, setError] = useState(null);
   
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(response => {
+        if(!response.ok) {
+          throw Error('Could not fetch the data for that resource');
+        }
       return response.json()
     })
       .then((data) => {
+        setError(null)
         setBlogs(data)
         setIsPending(false);
+      })
+      .catch((err) => {
+        setIsPending(false)
+        setError(err.message)
       })
   }, []);
 
   return ( 
     <div className="home">
+      {error && <div>{ error }</div> }
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       
