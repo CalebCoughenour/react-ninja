9de3cfb59b1576de14ac243960154e38ad7d1cc7
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-20 14:31:53 -0700

    demonstrate how filter is used by separating a specific author

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,15 @@ import BlogList from './BlogList';
 const Home = () => {
 
   const [blogs, setBlogs] = useState([
-    { title: 'Example Title1', body: 'Lorem ipsum...1', author: 'Example Author1', id: 1},
+    { title: 'Example Title1', body: 'Lorem ipsum...1', author: 'Belac', id: 1},
     { title: 'Example Title2', body: 'Lorem ipsum...2', author: 'Example Author2', id: 2},
-    { title: 'Example Title3', body: 'Lorem ipsum...3', author: 'Example Author3', id: 3}
+    { title: 'Example Title3', body: 'Lorem ipsum...3', author: 'Belac', id: 3}
   ]);
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs"/>
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'Belac')} title="Belac's Blogs"/>
     </div>
    );
 }
