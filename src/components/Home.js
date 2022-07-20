b9b646165bb21e746b2f4d511146252898a36f84
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-20 14:41:54 -0700

    add delete handler for blog posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,15 @@ const Home = () => {
     { title: 'Example Title3', body: 'Lorem ipsum...3', author: 'Belac', id: 3}
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Belac')} title="Belac's Blogs"/>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Belac')} title="Belac's Blogs"/> */}
     </div>
    );
 }
