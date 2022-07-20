558f1f47794f5badd3d1338ba0136dd3b86d678b
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-20 14:51:40 -0700

    add useEffect and add parameter for only running on name change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
+
+  const [name, setName] = useState('Belac');
 
   const [blogs, setBlogs] = useState([
     { title: 'Example Title1', body: 'Lorem ipsum...1', author: 'Belac', id: 1},
@@ -14,9 +16,16 @@ const Home = () => {
     setBlogs(newBlogs);
   }
 
+  useEffect(() => {
+    console.log('use effect ran');
+    console.log(name)
+  }, [name]);
+
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('Caleb')}>Change Name</button>
+      <p>{ name }</p>
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Belac')} title="Belac's Blogs"/> */}
     </div>
    );
