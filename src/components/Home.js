1a602d21aa3395d4c329cbb81bb73eff13b69645
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-07-20 14:26:54 -0700

    add bloglist component and pass props from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,7 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-
-// const [name, setName] =  useState('Belac');
-// const [age, setAge] = useState(29);
-  
-
-//   const handleClick = () => {
-//     setName('Caleb');
-//     setAge(30);
-//   }
 
   const [blogs, setBlogs] = useState([
     { title: 'Example Title1', body: 'Lorem ipsum...1', author: 'Example Author1', id: 1},
@@ -19,15 +11,7 @@ const Home = () => {
 
   return ( 
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
-      {/* <p>{ name } is { age }</p>
-      <button onClick={handleClick}>Click Me</button> */}
-
+      <BlogList blogs={blogs} title="All Blogs"/>
     </div>
    );
 }
